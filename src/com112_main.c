264929c1f55f80451d926aa167d0f9c3af02ba7f
Guilherme <guilherme.bort@outlook.com>
2020-05-25 17:39:52 -0300

    Add menu function

--- src/com112_main.c
+++ src/com112_main.c
@@ -4,5 +4,35 @@ int menu();
 int relatorio();
 
 int main(int argc, char const *argv[]) {
+	menu();
 	return 0;
+}
+
+int menu() {
+	int op = 0;
+	do {
+		printf(" 1. Bubble Sort\n");
+		printf(" 2. Selection Sort\n");
+		printf(" 3. Insertion Sort\n");
+		printf(" 4. Relatorio\n");
+		printf("-1. Sair\n");
+		scanf("%d", &op);
+
+		switch (op) {
+			case 1: {
+				printf("OP = %d!\n");
+			} break;
+			case 2: {
+				printf("OP = %d!\n");
+			} break;
+			case 3: {
+				printf("OP = %d!\n");
+			} break;
+			case -1: {
+			} break;
+			default: {
+				op = -1;
+			} break;
+		}
+	} while (op != -1);
 }
