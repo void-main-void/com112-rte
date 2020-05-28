3d909214e1bce4a2da0232a9ef9d93ea38dd78e9
Guilherme <guilherme.bort@outlook.com>
2020-05-28 16:33:18 -0300

    Add bubble sort optimization

--- src/com112_sort.c
+++ src/com112_sort.c
@@ -1,12 +1,21 @@
+void swap(int *a, int *b) {
+    int temp = 0;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void bubble_sort(int array[], int size) {
-    int foo = 0;
+    int swapped = 0;
     for (int i = 0; i < size - 1; ++i) {
         for (int j = i + 1; j < size; ++j) {
             if (array[j] < array[i]) {
-                foo = array[j];
-                array[j] = array[i];
-                array[i] = foo;
+                swap(&array[i], &array[j]);
+                swapped = 1;
             }
+        }
+        if (!swapped) {
+            break;
         }
     }
 }
--- src/com112_sort.h
+++ src/com112_sort.h
@@ -1,6 +1,7 @@
 #ifndef COM112_SORT_H
 #define COM112_SORT_H
 
+void swap(int *a, int *b);
 void bubble_sort(int array[], int size);
 
 #endif
