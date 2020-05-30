11a49d6a66ce13082f0645edc90ce11d99e8b1b6
Guilherme <guilherme.bort@outlook.com>
2020-05-29 22:19:38 -0300

    Fix counters

--- src/com112_sort.c
+++ src/com112_sort.c
@@ -1,3 +1,5 @@
+#include "com112_sort.h"
+
 void swap(int *a, int *b) {
     int temp = 0;
     temp = *a;
@@ -5,52 +7,54 @@ void swap(int *a, int *b) {
     *b = temp;
 }
 
-int *bubble_sort(int array[], int size) {
-    int counter[2] = { 0 }; // 0 = swaps  ||  1 = comparisons
+sortinfo bubble_sort(int array[], int size) {
+    sortinfo counter;
+    counter.comparisons = counter.swaps = 0;
     for (int i = 0; i < size - 1; ++i) {
         for (int j = i + 1; j < size; ++j) {
             if (array[j] < array[i]) {
                 swap(&array[i], &array[j]);
-                ++counter[0];
-                ++counter[1];
+                ++counter.swaps;
             }
+            ++counter.comparisons;
         }
     }
     return counter;
 }
 
-int *selection_sort(int array[], int size) {
+sortinfo selection_sort(int array[], int size) {
     int min_index = 0;
-    int counter[2] = { 0 }; // 0 = swaps  ||  1 = comparisons
+    sortinfo counter;
     for (int i = 0; i < size - 1; ++i) {
         min_index = i;
         for (int j = i + 1; j < size; ++j) {
             if (array[j] < array[min_index]) {
                 min_index = j;
-                ++counter[1];
             }
+            ++counter.comparisons;
         }
         swap(&array[i], &array[min_index]);
-        ++counter[0];
+        ++counter.swaps;
     }
     return counter;
 }
 
-int *insertion_sort(int array[], int size) {
+sortinfo insertion_sort(int array[], int size) {
     int key = 0;
     int j = 0;
-    int counter[2] = { 0 }; // 0 = swaps  ||  1 = comparisons
+    sortinfo counter;
+    counter.comparisons = counter.swaps = 0;
     for (int i = 1; i < size; ++i) {
         key = array[i];
         j = i - 1;
         while (j >= 0 && key < array[j]) {
             array[j + 1] = array[j];
-            ++counter[0];
-            ++counter[1];
+            ++counter.swaps;
             --j;
         }
+        ++counter.comparisons;
         array[j + 1] = key;
-        ++counter[0];
+        ++counter.swaps;
     }
     return counter;
 }
--- src/com112_sort.h
+++ src/com112_sort.h
@@ -1,9 +1,14 @@
 #ifndef COM112_SORT_H
 #define COM112_SORT_H
 
+typedef struct sortinfo_t {
+    int comparisons;
+    int swaps;
+} sortinfo;
+
 void swap(int *a, int *b);
-int bubble_sort(int array[], int size);
-int selection_sort(int array[], int size);
-int insertion_sort(int array[], int size);
+sortinfo bubble_sort(int array[], int size);
+sortinfo selection_sort(int array[], int size);
+sortinfo insertion_sort(int array[], int size);
 
 #endif
