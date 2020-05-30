da8c06319ffa02655c32c188daa10d23f69ef65c
Guilherme <guilherme.bort@outlook.com>
2020-05-29 22:19:28 -0300

    Add write report feature

--- src/com112_file.c
+++ src/com112_file.c
@@ -11,3 +11,20 @@ int write_io_file(FILE *fp, const char file_name[], int size, int *array) {
     fp = 0;
     return 1;
 }
+
+FILE *create_report(FILE *fp, int size) {
+    fp = fopen("com112_relatorio.txt", "w");
+    fprintf(fp, "Número de elementos ordenados: %d\n", size);
+    return fp;
+}
+
+void end_report(FILE *fp) {
+    fclose(fp);
+}
+
+void write_sort_info(FILE *fp, const char *description, sortinfo info, double time) {
+    fprintf(fp, description);
+    fprintf(fp, "\tTempo de execução: %f.\n", time);
+    fprintf(fp, "\tNúmero de comparações: %d.\n", info.comparisons);
+    fprintf(fp, "\tNúmero de movimentações: %d.\n", info.swaps);
+}
--- src/com112_file.h
+++ src/com112_file.h
@@ -4,7 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "com112_sort.h"
 
 int write_io_file(FILE *fp, const char file_name[], int size, int *array);
-
+FILE *create_report();
+void end_report(FILE *fp);
+void write_sort_info(FILE *fp, const char *description, sortinfo info, double time);
 #endif
